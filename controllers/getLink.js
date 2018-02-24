0e3cd7291ce94b21649437b56d527a7d6c0411fc
Matt Wiseman <mrslwiseman@gmail.com>
2018-02-24 12:50:12 +1100

    refactor comments and variable dec

--- controllers/getLink.js
+++ controllers/getLink.js
@@ -1,23 +1,20 @@
-const express = require('express')
-    , router = express.Router({ mergeParams: true })
-    , mongoose = require('mongoose')
-    , Link = require('../models/Link');
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const mongoose = require('mongoose');
+const Link = require('../models/Link');
 
 router.get('/', (req, res) => {
     (async function () {
-        // const dbName = 'shrinkthis';
-        // const url = `mongodb://localhost:27017/${dbName}`;
         try {
-            // TODO: check is a valid
-            // await mongoose.connect(url);
-            // TODO: ok for small scale, not good if we need letters
+            // TODO: ok for small scale
             const shortLinkId = Number(req.params.id);
             const search = await Link.findOne({ id: shortLinkId });
+
             if (search === null) throw Error('Link Id was invalid or not found.');
 
             // TODO: incremement the link hit counter array with a timestamp
 
-            // we're done. 
+            // Over 'n' out. 
             res.redirect(search.url);
         } catch (e) {
             console.log(e);
