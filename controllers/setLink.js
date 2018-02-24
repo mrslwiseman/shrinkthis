e7d6b6662b3ff5ca994e76bd034a78d5b69bb41c
Matt Wiseman <mrslwiseman@gmail.com>
2018-02-24 12:54:24 +1100

    white space / comment

--- controllers/setLink.js
+++ controllers/setLink.js
@@ -42,6 +42,7 @@ router.get('/', (req, res) => {
             })
         }
     })();
+    
     });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/favicon.ico', (req,res) => res.status(204).end())
 app.use('/new', setLink) // @query url=http://www.someUrl.com
 app.use('/:id', getLink) // @number
-app.use('/', index) // @number
+app.use('/', index) // TODO: Point to a front end form
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`)
