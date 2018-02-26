024b357b20a0888ecfbd1059d19af599e7c8bbd1
Matt Wiseman <mrslwiseman@gmail.com>
2018-02-26 11:11:39 +1100

    separate helper methods

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,9 +1,22 @@
-exports.isValidUrl = (link) => {
-    const re = /^(http:\/\/)|^(https:\/\/)/gi; // check starts with http(s)://
-    return link.match(re)
+exports.urlIsValid = (link) => {
+    const re = /^((?:https?|ftp):\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
+    return re.test(link);
 }
 
-exports.formatURL = (link) => {
-    const cleanLink = 'http://' + link.replace(/ /gi, ''); // remove any white space
+exports.urlHasWhiteSpace = (link) => {
+    return (/ /gi).test(link);
+}
+
+exports.removeWhiteSpace = (link) => {
+    const cleanLink = link.replace(/ /gi, ''); // remove any white space
     return cleanLink;
+}
+
+exports.urlHasProtocol = (link) => {
+    const re = /^(http:\/\/)|^(https:\/\/)/gi; // check starts with http(s)://
+    return re.test(link)
+}
+
+exports.addUrlProtocol = (link) => {
+    return 'http://' + link
 }
